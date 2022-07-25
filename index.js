b3463347c79621a1387ff2932c95c1f5d07ccf1b
Satria Alfarizki <107311445+Satriaalfa123@users.noreply.github.com>
2022-07-26 00:00:22 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,122 @@
+class Game {
+    constructor() {
+        this.game = [
+            {
+                id: '1',
+                name: 'batu',
+                Image: 'assets/batu.png',
+            },
+            {
+                id: '2',
+                name: 'kertas',
+                Image: 'assets/kertas.png',
+            },
+            {
+                id: '3',
+                name: 'gunting',
+                Image: 'assets/gunting.png',
+            },
+            // {
+            //     id: '1',
+            //     name: 'batu'
+            //     image: 'assets/batu.png',
+            // },
+            // {
+            //     id: '2',
+            //     name: 'kertas',
+            //     image: 'assets/kertas.png',
+            // }
+            // {
+            //     id: '3',
+            //     name: 'gunting',
+            //     image: src='assets/gunting.png'
+            // },
+        ]
+        this.clicked = ''
+        this.comCliked = ''
+        this.comname = ''
+    }
+    getRandomInt(min, max) {
+        min = Math.ceil(min)
+        max = Math.floor(max)
+        return Math.floor(Math.random()* (max - min) + min)
+    }
+    randomPick(gameArr) {
+        let timeleft = 3
+        let downloadTimer = setInterval(function(){
+            timeleft -= 1
+            this.comCliked = `com-${gameArr[timeleft].name}-${gameArr[timeleft].id}`
+            const elem = document.getElementById(this.comClicked)
+            elem.classList.add(`click-player-content`)
+            elem.classList.remove('default-content')
+            setTimeout(()=> {
+                elem,classList.add('default-content')
+                elem.classList.remove('click-player-content')
+            }, 1000)
+            if (timeleft <= 0){
+                clearInterval(downloadTimer)
+            }
+        }, 1000)
+    }
+    winner(player, computer) {
+        const elemCom = document.getElementById('versus')
+        elemCom.classList.add('div-vs')
+        if (player === computer) {
+            elemCom.innerHTML = '<h3 class"text-result">DRAW</h3>'
+        } else {
+            if (player == 'batu') {
+                if (computer == 'gunting') {
+                    elemCom.innerHTML = '<h3 class="text-result">You Win</h3>'
+                } else {
+                    elemCom.innerHTML = '<h3 class="text-result"> You Lose</h3>'
+                }
+            } else if (player === 'kertas') {
+                if (computer === 'batu') {
+                    elemCom.innerHTML = '<h3 class="text-result">You Win</h3>'
+                } else {
+                    elemCom.innerHTML = '<h3 class="text-result"> You Lose</h3>'
+                }
+            } else if (player === 'gunting') {
+                if (computer === 'kertas') {
+                    elemCom.innerHTML = '<h3 class="text-result">You Win</h3>'
+                } else {
+                    elemCom.innerHTML = '<h3 class="text-result"> You Lose</h3>'
+                }
+            }
+        }
+    }
+    hadleClick(id, name) {
+        this.clicked = id
+        const elem = document.getElementById(id)
+        elem.classList.add('click-player-content')
+        elem.classList.remove('default-content')
+        this.randomPick(this.game)
+        setTimeout(() => {
+            const indexCom = this.getRandomInt(0, 3)
+            this.comCliked = `com-${this-game[indexCom].name}-${this.game[indexCom].id}`
+            this.comName = this-game[indexCom].name
+            const elemCom = document.getElementById(this.comCliked)
+            elemCom.classList.add('click-player-content')
+            elemCom.classList.remove('default-content')
+            this.winner(name, this.comName)
+        }, 5000)
+    }
+    refresh() {
+        document.location.reload()
+    }
+    play() {
+        let player = '<h1>PLAYER 1</h1>'
+
+        for (let i = 0;i < this.game.length; i++) {
+            player += `<div id="player-${this.game[i].name}-${this.game[i].id}" class="default-content" onclick="gaming.handleClick('player-${this.game[i].name}-${this.game[i].id}','${this.game[i].name}')"><img src="./assets/${this.game[i].image}" alt="${this.game[i].name}" class="image-game"></div>`
+        }
+        let com = '<h1>COM</h1>'
+        for (let i = 0; i <this.game.length; i++)
+            com += `<div id="com-${this.game[i].name}-${this.game[i].id}" class="default-content"><img src="./assets/${this.game[i].image}" alt="${this.game[i].name}" class="image-game"></div>`
+    }
+    //     document.getElementById('player-content').innerHTML = player
+    //     document.getElementById('com-content').innerHTML = com
+    // }
+}
+const gaming = new Game()
+gaming.play()
